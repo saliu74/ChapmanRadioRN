0b5200267ceb9238feecc2b342a4106689cc3a95
samy achour <sachour96@gmail.com>
2016-11-02 02:11:27 -0700

    Changed color of play button

--- src/ChapmanRadioRN.js
+++ src/ChapmanRadioRN.js
@@ -27,7 +27,7 @@ const PlayButton = MKButton.coloredFab()
       width: 100,
       height: 100
    })
-  .withBackgroundColor(MKColor.Cyan)
+  .withBackgroundColor('#92f7fc')
   .withStyle({
         shadowRadius: 8,
         shadowOffset: { width: 0, height: 0.5 },
@@ -162,7 +162,8 @@ class ChapmanRadioRN extends Component {
 
         this.setState({
 
-          songText: "No song playing currently"
+          songText: "No song playing currently",
+          songPic: "."
 
         });
 
