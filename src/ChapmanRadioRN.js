151de600529576d30e5ac8607608f0548cb0a0dd
samy achour <sachour96@gmail.com>
2017-04-16 22:18:14 -0700

    added comments!

--- src/ChapmanRadioRN.js
+++ src/ChapmanRadioRN.js
@@ -16,14 +16,14 @@ import {
   MKColor,
   MKButton,
   getTheme
-} from 'react-native-material-kit';
+} from 'react-native-material-kit'; // My UI components - the play button and show/schedule cards
 
 import ScrollableTabView, {
   ScrollableTabBar,
   DefaultTabBar
-} from 'react-native-scrollable-tab-view';
+} from 'react-native-scrollable-tab-view'; // My 2 tab interface - Player/Schedule
 
-import { ReactNativeAudioStreaming, Player } from 'react-native-audio-streaming';
+import { ReactNativeAudioStreaming, Player } from 'react-native-audio-streaming'; // The library I used to stream the audio
 const url = "http://198.175.251.242/listen";
 
 // Play button specifics
@@ -49,7 +49,7 @@ class ChapmanRadioRN extends Component {
         super(props);
 
         this.state = {
-          playButtonLabel: " PLAY ",
+          playButtonLabel: " PLAY ", // the spaces around play indicate that this is the first time the user opened the app, they aren't reopening it after minimizing
           showJSON: "",
           showPic: ".",
           showText: "",
@@ -84,7 +84,7 @@ class ChapmanRadioRN extends Component {
               >
                 <View tabLabel='Player' style={Style.rootContainer}>
 
-
+                  {/* The Player tab */}
                   <View style={Style.playContainer}>
                     <PlayButton
                       onPress={() => {
@@ -95,6 +95,7 @@ class ChapmanRadioRN extends Component {
                     </PlayButton>
                   </View>
                   <ScrollView style={Style.cardContainer}>
+                    {/* Show card */}
                     <View style={Style.card1}>
                       <View style={theme.cardStyle}>
                         <Image source={{uri : this.state.showPic}} style={Style.cardImageStyle} />
@@ -103,6 +104,7 @@ class ChapmanRadioRN extends Component {
                         </Text>
                       </View>
                     </View>
+                    {/* Song card */}
                     <View style={Style.card2}>
                       <View style={theme.cardStyle}>
                         <Image source={{uri : this.state.songPic}} style={Style.cardImageStyle} />
@@ -118,6 +120,7 @@ class ChapmanRadioRN extends Component {
 
 
               <View tabLabel='Schedule' style={Style.scheduleRootContainer}>
+                {/* Search Bar */}
                 <TextInput
                   style={Style.searchField}
                   onChangeText={(text) => this.makeSchedule(text)}
@@ -126,6 +129,7 @@ class ChapmanRadioRN extends Component {
                   underlineColorAndroid='transparent'
                 />
                 <ScrollView style={Style.scheduleContainer}>
+                  {/* Dynamically created list of schedule cards */}
                   {this.state.scheduleView}
                 </ScrollView>
               </View>
@@ -137,21 +141,25 @@ class ChapmanRadioRN extends Component {
     }
 
     _onPlayButtonPressed() {
+      // Adds a nice animation
       LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
-      // LayoutAnimation.configureNext(LayoutAnimation.Presets.linear);
-      // LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+
+      // If it's the first time the user opened the app and played the stream
       if (this.state.playButtonLabel == " PLAY ") {
         ReactNativeAudioStreaming.stop();
         ReactNativeAudioStreaming.play(url, {showIniOSMediaCenter: true, showInAndroidNotifications: true});
         this.setState({
             playButtonLabel: "PAUSE"
         })
-      } else if (this.state.playButtonLabel == "PLAY") {
+      }
+      // If the user is reopening the app after minimizing and playing the stream
+      else if (this.state.playButtonLabel == "PLAY") {
         ReactNativeAudioStreaming.resume();
         this.setState({
           playButtonLabel: "PAUSE"
         })
       }
+      // If the user is unpausing the stream at any point in use
       else {
         ReactNativeAudioStreaming.pause();
         this.setState({
@@ -160,6 +168,7 @@ class ChapmanRadioRN extends Component {
       }
     }
 
+    // Populate the schedule and set a timer to refresh the show every 1000 milliseconds
     componentWillMount() {
       this.getSchedule()
       setInterval(() => {
@@ -173,16 +182,19 @@ class ChapmanRadioRN extends Component {
 
     }
 
-    // Populates some render thing with views to create schedule scroll view with material cards
+    // Populates scheduleView with views to create schedule scroll view with material cards
     makeSchedule(searchStr) {
 
       const theme = getTheme();
       var evens = true
 
+      // If the user didn't search anything, and we're just populating the schedule for the first time
       if (searchStr == "") {
 
+          // Takes in a bunch
           var contents = this.state.scheduleArray.map(function (item) {
 
+              // If we need a new section head, ie a weekday
             	if (evens) {
                 evens = false
 
@@ -190,11 +202,15 @@ class ChapmanRadioRN extends Component {
                   <Text style={Style.sectionHead} key={Math.random()}>{item}</Text>
                 );
               }
+              // If we're populating schedule cards, not a section head (weekday)
             	else {
                 evens = true
 
+                // Item is a list of traits for a scheduled show, title, description, image link, etc.
                 var contentsTemp = item.map(function (item) {
                   var scheduleText = "(" + item[3] + ") " + item[2] + ": " + item[1] + " (" + item[4] + ")"
+                  // Dynamically create a schedule card with our item
+                  // Each card NEEDS a unique key so we use Math.random()
                   return (
                     <View style={Style.scheduleCard} key={Math.random()}>
                       <View style={theme.cardStyle}>
@@ -212,11 +228,11 @@ class ChapmanRadioRN extends Component {
               }
            });
 
-     } else {
+     } else { // If the user searched something
 
        var contents = this.state.scheduleArray.map(function (item) {
 
-         if (evens) {
+         if (evens) { // Ignore this, there are no section heads in search results
            evens = false
          }
 
@@ -224,10 +240,12 @@ class ChapmanRadioRN extends Component {
            evens = true
 
            var contentsTemp = item.map(function (item) {
-
+             // Item is a list of traits for a scheduled show, title, description, image link, etc.
              if ((item[1] + item[2] + item[3]).toLowerCase().includes(searchStr.toLowerCase())) {
 
                var scheduleText = "(" + item[3] + ") " + item[2] + ": " + item[1] + " (" + item[4] + ")"
+               // Dynamically create a schedule card with our item
+               // Each card NEEDS a unique key so we use Math.random()
                return (
                  <View style={Style.scheduleCard} key={Math.random()}>
                    <View style={theme.cardStyle}>
@@ -242,7 +260,8 @@ class ChapmanRadioRN extends Component {
              }
 
              else {
-
+               // Since we still need to return a card if it doesn't fall into the search results...
+               // We return a discarded card with height 0 (see Style.js)
                return (
                  <View style={Style.discardCard} key={Math.random()}>
                  </View>
@@ -257,7 +276,7 @@ class ChapmanRadioRN extends Component {
 
            }
 
-             //end of contentsTemp
+             //end of contentsTemp, return this given section head/card/discardCard
              return (
                contentsTemp
              );
@@ -265,18 +284,20 @@ class ChapmanRadioRN extends Component {
         });
 
      }
+       // This actually creates the schedule using all our dynamically generated components
        this.state.scheduleView = contents
 
     }
 
     getSchedule() {
 
+      // Grabs the schedule JSON data and populates the schedule for the first time
       fetch("https://api.chapmanradio.com/legacy/schedule.json")
         .then((response) => response.json())
         .then((responseData) => {
 
           for (var i in responseData) {
-            this.state.scheduleArray.push(responseData[i].title)
+            this.state.scheduleArray.push(responseData[i].title) // Title is the day of the week
             this.state.scheduleArray.push(responseData[i].data)
           }
 
@@ -287,7 +308,7 @@ class ChapmanRadioRN extends Component {
 
     }
 
-    refresh() {
+    refresh() { // Refresh the live player view
 
       fetch("http://api.chapmanradio.com/legacy/livestreams.json")
         .then((response) => response.json())
@@ -297,6 +318,7 @@ class ChapmanRadioRN extends Component {
           this.setState({songJSON: responseData.nowplaying});
           this.setState({showJSON: responseData.show});
 
+          // If the show changed
           if (this.state.oldShowJSON != this.state.showJSON) {
 
             // Live Show
@@ -312,7 +334,7 @@ class ChapmanRadioRN extends Component {
 
             }
 
-            // Automation
+            // Automation, no show+song
 
             if (this.state.showJSON.showname == null) {
 
@@ -329,6 +351,7 @@ class ChapmanRadioRN extends Component {
 
           }
 
+        // New song
         if (this.state.oldSongJSON != this.state.songJSON && this.state.showJSON.showname != null) {
 
           // Song
@@ -371,6 +394,7 @@ class ChapmanRadioRN extends Component {
         })
         .done();
 
+        // Gets the stream status, sets the play button label dynamically
         ReactNativeAudioStreaming.getStatus((err,r) =>
         {
           if ((r.status == "STOPPED"|| r.status == "PAUSED") && this.state.playButtonLabel == "PAUSE")
